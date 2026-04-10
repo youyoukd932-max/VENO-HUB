b4f5cb02409196297d8a6126a418650c3c2a16db
youyoukd932-max <youyoukd932@gmail.com>
2026-04-10 23:58:10 +0200

    Create VenoHub.lua

---
+++ VenoHub.lua
@@ -0,0 +1,134 @@
+local RixerLibrary = loadstring(game:HttpGet(('https://raw.githubusercontent.com/cnPthPiGon/CM9/refs/heads/main/Library.lua')))()
+
+local Window = RixerLibrary:MakeWindow({
+    Name = "TOMAR HUB                         NO ACCES TO SKID",
+    HidePremium = false,
+    SaveConfig = true,
+    ConfigFolder = "DestroyerHub" -- Met un nom personnalisé pour ton hub ici
+})
+
+print("the owner of the scripts is rixer95-x2 in youtube.") -- Just a debug
+
+local Tab1 = Window:MakeTab({
+    Name = "THEIR PS",
+    Icon = "rbxassetid://117917068601552",
+    PremiumOnly = false
+})
+
+Tab1:AddButton({
+    Name = "OMAHAD (INSTANT STEAL V2 NEED CARPET)",
+    Callback = function()
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/f21b5c7f24a92cef3a728c297b7ba2bc.lua"))()
+    end    
+})
+
+Tab1:AddButton({
+    Name = "NAMELESS HUB",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ily123950/Vulkan/refs/heads/main/Tr"))()
+    end    
+})
+
+Tab1:AddButton({
+    Name = "SEMI TP (NEED CARPET)",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Tokinu/Semi-TP/refs/heads/main/Free"))()
+    end    
+})
+
+Tab1:AddButton({
+    Name = "LEAVE BUTTON",
+    Callback = function()
+        loadstring(game:HttpGet("https://pastefy.app/avJfNdOe/raw"))()
+    end    
+})
+
+Tab1:AddButton({
+    Name = "KANYE ALLOW DISALOW",
+    Callback = function()
+        loadstring(game:HttpGet("https://pastefy.app/oYbSASyd/raw"))()
+    end    
+})
+
+Tab1:AddButton({
+    Name = "AP SPAMMER",
+    Callback = function()
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/66d9db24d2ae4b45dd330bfd433ecbb7.lua"))()
+    end    
+})
+
+local Tab2 = Window:MakeTab({
+    Name = "MY PS",
+    Icon = "rbxassetid://117917068601552",
+    PremiumOnly = false
+})
+
+Tab2:AddButton({
+    Name = "AP PROTECT",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/themk240/tmk-hub/refs/heads/main/apbaseprotectornokick"))()
+    end    
+})
+
+Tab2:AddButton({
+    Name = "TP BLOCK (NEED CARPET)",
+    Callback = function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/60hZyzUa"))()
+    end    
+})
+
+Tab2:AddButton({
+    Name = "SUN AUTO GRAB(MOBILE)",
+    Callback = function()
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/c5ed96fe96878d4c553e836e674afb17.lua"))()
+    end    
+})
+
+Tab2:AddButton({
+    Name = "AUTO BLOCK FAST",
+    Callback = function()
+        loadstring(game:HttpGet("https://pastefy.app/v34Tt4oq/raw"))()
+    end    
+})
+
+Tab2:AddButton({
+    Name = "FLASH TP TIGY",
+    Callback = function()
+        loadstring(game:HttpGet("https://ufwxzxvpnpvzkthfujyp.supabase.co/functions/v1/serve-script?id=113"))()
+    end    
+})
+
+Tab2:AddButton({
+    Name = "AP PROTECT",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/themk240/tmk-hub/refs/heads/main/apbaseprotectornokick"))()
+    end    
+})
+
+local Tab3 = Window:MakeTab({
+    Name = "DUEL",
+    Icon = "rbxassetid://117917068601552",
+    PremiumOnly = false
+})
+
+Tab3:AddButton({
+    Name = "K7 Duel",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/mallu937/K7-DUEL/refs/heads/main/.lua"))()
+    end    
+
+})
+
+Tab3:AddButton({
+    Name = "ICE LAGGER DUEL",
+    Callback = function()
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/223c2c219f3caa6e6700ae93511b94ab.lua"))()
+    end    
+})
+
+Tab3:AddButton({
+    Name = "KURD HUB (DESYNC)",
+    Callback = function()
+        loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Kurd-hub-51808"))()
+    end    
+})
